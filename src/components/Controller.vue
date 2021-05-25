522039ef55648a4ca2731f5cec848796a191f795
Niels Houben <niels.houben@gmail.com>
2021-05-25 23:39:34 +0200

    no progress

---
+++ src/components/Controller.vue
@@ -0,0 +1,35 @@
+<template>
+  <input
+    type="range"
+    id="slider"
+  >
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#slider {
+  -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
+  width: 100%; /* Specific width is required for Firefox. */
+  background: transparent; /* Otherwise white in Chrome */
+  padding: 5px;
+  border-bottom: 3px solid black;
+}
+
+#slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  height: 6vw;
+  width: 4vw;
+  background: rgb(84, 216, 31);
+  border: 0.4vw solid black;
+  border-radius: 10px;
+}
+
+#slider:focus {
+  outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,62 @@
 <template>
-  <div>
+  <div id="container">
     <Slider
       v-model="value"
-      min="0"
-      max="201"
+      :min="0"
+      :max="201"
+      :step="1"
+      :direction="direction"
+      :format="format"
+      @update="update"
     />
+    <br>
+    <br>
+    <input v-model="value">
+    <br>
+    <input v-model="direction">
+
+    <br>
+    <br>
+    <br>
+    <br>
+    -------------------------------
+    <br>
+    <br>
+    <br>
+    <br>
+    <Controlls></Controlls>
   </div>
 </template>
 
 <script>
 import Slider from '@vueform/slider'
+import Controlls from '@/components/Controller.vue'
 
 export default {
-  components: {
-    Slider,
-  },
-  data () {
-    return {
-      value: 100
-    }
+  components: { Slider, Controlls },
+  data: () => ({
+    power: 0,
+    value: 101,
+    direction: "ltr",
+  }),
+  methods: {
+    update (value) {
+      console.log(value)
+      if (value == 101) { this.power = 0 }
+      else this.power = value > 100 ? `${value - 100}` : `-${101 - value}`
+      if (value > 101) { this.direction = 'ltr' }
+      else if (value < 101) { this.direction = 'rtl' }
+    },
+    format () { return `${this.power}%` }
   }
 }
 </script>
 
+<style >
+#container {
+  width: 75%;
+  padding-top: 45px;
+  margin: 0 auto;
+}
+</style>
 <style src="@vueform/slider/themes/default.css"></style>
